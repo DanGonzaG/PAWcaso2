502a81efcaec70f2499b8794ac0afb613c83c306
jpiedra50401 <75505128+jpiedra50401@users.noreply.github.com>
2025-04-19 10:20:48 -0600

    Removed api references

--- G4_CasoEstudio2/G4_CasoEstudio2.App/obj/Debug/net8.0/G4_CasoEstudio2.App.AssemblyInfo.cs
+++ G4_CasoEstudio2/G4_CasoEstudio2.App/obj/Debug/net8.0/G4_CasoEstudio2.App.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("G4_CasoEstudio2.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d46dc80a39563b3233bbf437015d2290fa11c13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5647833f8ae808a44bd42bdc37c7c60dd41edd9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("G4_CasoEstudio2.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("G4_CasoEstudio2.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
