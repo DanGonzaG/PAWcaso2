3d4ae8439db8625a0bffaafd0066fae979e6bb55
jpiedra50401 <75505128+jpiedra50401@users.noreply.github.com>
2025-04-21 20:52:45 -0600

    Dashboard funcando

---
+++ G4_CasoEstudio2/G4_CasoEstudio2.App/Controllers/DashboardController.cs
@@ -0,0 +1,44 @@
+﻿
+using G4_CasoEstudio2.App.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace G4_CasoEstudio2.App.Controllers
+{
+    [Authorize(Roles = "Administrador")]
+    public class DashboardController : Controller
+    {
+        private readonly Contexto _context;
+
+        public DashboardController(Contexto context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var totalUsuarios = await _context.Usuarios.CountAsync();
+            var totalEventos = await _context.Eventos.CountAsync();
+            var totalAsistencias = await _context.Asistencias.CountAsync();
+
+            var topEventos = await _context.Asistencias
+                .GroupBy(a => a.Evento)
+                .Select(g => new
+                {
+                    Evento = g.Key,
+                    Total = g.Count()
+                })
+                .OrderByDescending(g => g.Total)
+                .Take(5)
+                .ToListAsync();
+
+            ViewBag.TotalUsuarios = totalUsuarios;
+            ViewBag.TotalEventos = totalEventos;
+            ViewBag.TotalAsistencias = totalAsistencias;
+            ViewBag.TopEventos = topEventos;
+
+            return View();
+        }
+    }
+}
--- G4_CasoEstudio2/G4_CasoEstudio2.App/obj/Debug/net8.0/G4_CasoEstudio2.App.AssemblyInfo.cs
+++ G4_CasoEstudio2/G4_CasoEstudio2.App/obj/Debug/net8.0/G4_CasoEstudio2.App.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("G4_CasoEstudio2.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+502a81efcaec70f2499b8794ac0afb613c83c306")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ac59569e2289087852b4060b5523d8273e47858")]
 [assembly: System.Reflection.AssemblyProductAttribute("G4_CasoEstudio2.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("G4_CasoEstudio2.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
